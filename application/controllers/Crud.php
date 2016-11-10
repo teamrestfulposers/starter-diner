ca6df0c55c8b1a405a18c80068ab231130baa1dc
Chiseong Oh <nornen@naver.com>
2016-11-10 15:06:46 -0800

    changed description part and added two buttons for save and cancel

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -21,6 +21,11 @@ class Crud extends Application {
         $this->data['items'] = $this->menu->all();
         $this->render();
     }
+    function cancel() {
+        $this->session->unset_userdata('key');
+        $this->session->unset_userdata('record');
+        $this->index();
+    }
 
     public function edit($id = null) {
         // try the session first
@@ -36,12 +41,17 @@ class Crud extends Application {
         // build the form fields
         $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
         $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
-        $this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
+        $this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
         $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
         $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
         $this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
+        $cats = $this->categories->all(); // get an array of category objects
+        foreach($cats as $code => $category) // make it into an associative array
+            $codes[$code] = $category->name;
+        $this->data['fcategory'] = makeCombobox('Category', 'category', $record->category,$codes);
         // show the editing form
         $this->data['pagebody'] = "mtce-edit";
+        $this->data['zsubmit'] = makeSubmitButton('Save', 'Submit changes');
         $this->render();
     }
 
