78c0cdfdd1abf98c951fcdff2fa19be62a860602
Dejack <>
2016-11-13 16:23:36 -0800

    finished ci-fun02b Signed-off-by:Dejack

--- application/views/mtce-edit.php
+++ application/views/mtce-edit.php
@@ -1,9 +1,18 @@
-<h2>Menu Maintenance - Editing</h2>
-<form action="#" method="post">
-{fid}
-{fname}
-{fdescription}
-{fprice}
-{fpicture}
-{fcategory}
+<h2>Menu Maintenance - {action}</h2>
+<form action="/crud/save" method="post" enctype="multipart/form-data">
+    
+    {error_messages}
+    {fid}
+    {fname}
+    {fdescription}
+    {fprice}
+    {fpicture}
+    <div class="form-group">
+        <label for="replacement">Replacement picture</label>
+        <input type="file" id="replacement" name="replacement"/>
+    </div>
+    {fcategory}
+    {zsubmit} <a class="btn btn-default" role="button" href="/crud/cancel">Cancel</a>
+    <a class="btn btn-default" role="button" href="/crud/delete">Delete</a>
+    
 </form>
