474e580d5e90a01187a98bdf868c53b2c2011fda
Chiseong Oh <nornen@naver.com>
2016-11-19 21:17:50 -0800

    finished 7 to 12 steps

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,18 +6,19 @@ class Welcome extends Application {
 
 	public function index()
 	{
+		$role = $this->session->userdata('userrole');
+                if($role==''){
+                    $this->session->set_userdata('userrole', 'user');
+                }
+
 		$result = '';
 		$oddrow = true;
-		foreach ($this->categories->all() as $category)
-		{
-//			$viewparms = array(
-//				'direction' => ($oddrow ? 'left' : 'right')
-//			);
-//			$viewparms = array_merge($viewparms, $category);
-			$category->direction = ($oddrow ? 'left' : 'right');
-			$result .= $this->parser->parse('category-home', $category, true);
-			$oddrow = ! $oddrow;
-		}
+
+		foreach ($this->categories->all() as $category){
+                    $category->direction = ($oddrow ? 'left' : 'right');
+                    $result .= $this->parser->parse('category-home', $category, true);
+                    $oddrow = ! $oddrow;
+                }
 		$this->data['content'] = $result;
 		$this->render();
 	}
